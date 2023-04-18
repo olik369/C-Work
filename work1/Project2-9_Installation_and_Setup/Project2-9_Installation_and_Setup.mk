79b891e95fc0eb851d12a4bbb1e71fe570c6b1f3
KimDongHo <olik369@gmail.com>
2023-04-19 01:41:14 +0900

    ##2-9 Installation and Setup

---
+++ work1/Project2-9_Installation_and_Setup/Project2-9_Installation_and_Setup.mk
@@ -0,0 +1,109 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=Project2-9_Installation_and_Setup
+ConfigurationName      :=Debug
+WorkspaceConfiguration :=Debug
+WorkspacePath          :=C:/Users/olik3/OneDrive/Desktop/works/work1
+ProjectPath            :=C:/Users/olik3/OneDrive/Desktop/works/work1/Project2-9_Installation_and_Setup
+IntermediateDirectory  :=$(ConfigurationName)
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=olik3
+Date                   :=19/04/2023
+CodeLitePath           :="C:/Program Files/CodeLite"
+LinkerName             :=C:/mingw64/bin/g++.exe
+SharedObjectLinkerName :=C:/mingw64/bin/g++.exe -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputDirectory        :=$(IntermediateDirectory)
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName).exe
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="Project2-9_Installation_and_Setup.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :="C:/Program Files/CodeLite/mkdir.exe" -p
+RcCmpOptions           := 
+RcCompilerName         :=C:/mingw64/bin/windres.exe
+LinkOptions            :=  -static
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overridden using an environment variable
+##
+AR       := C:/mingw64/bin/ar.exe -r
+CXX      := C:/mingw64/bin/g++.exe
+CC       := C:/mingw64/bin/gcc.exe
+CXXFLAGS :=  -O0 -gdwarf-2 -std=c++17 -Wall $(Preprocessors)
+CFLAGS   :=  -gdwarf-2 -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := C:/mingw64/bin/as.exe
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=C:\Program Files\CodeLite
+Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@$(MakeDirCommand) "$(ConfigurationName)"
+
+
+$(IntermediateDirectory)/.d:
+	@$(MakeDirCommand) "$(ConfigurationName)"
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/olik3/OneDrive/Desktop/works/work1/Project2-9_Installation_and_Setup/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
+
+##
+## Clean
+##
+clean:
+	$(RM) -r $(ConfigurationName)/
+
+
