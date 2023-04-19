3a4045e52c5c8f41de686520a50b63f61b3a9fdb
KimDongHo <olik369@gmail.com>
2023-04-20 00:07:25 +0900

    ##4-29 What are Compilers Errors?

---
+++ Section4/FirstProgram/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int favorite_number;
+    cout << "Enter your favorite number between 1 and 100 : ";
+    cin >> favorite_number;
+    cout << "Amazing!! That's my favorite number too!" << endl;
+    return 0;
+}
