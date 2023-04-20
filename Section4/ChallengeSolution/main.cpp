ae608b8f2ed34439f49f46c6c676bd809e7c86c4
KimDongHo <olik369@gmail.com>
2023-04-20 22:47:28 +0900

    #Section4 end

---
+++ Section4/ChallengeSolution/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int favoriteNumber;
+    
+    cout << "Enter your favorite number between 1 and 100: ";
+    cin >> favoriteNumber;
+    cout << "Amazing!! That's my favorite number too!" << endl;
+    cout << "No really!!, " << favoriteNumber << " is my favorite number!" << endl;
+    
+    return 0;
+}
---
+++ Section4/CompilerWarnings/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int favorite_number;
+    
+    cout << "Hello World" << endl;
+    return 0;
+}
